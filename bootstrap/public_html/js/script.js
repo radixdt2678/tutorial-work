286e419e1c17bc7b3038fb22ba80a1328680938b
radixdt2678 <radixdt2678@gmail.com>
2023-02-10 12:02:56 +0530

    Now Added file

---
+++ bootstrap/public_html/js/script.js
@@ -0,0 +1,46 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    loop: true,
+    centerSlide: 'true',
+    fade:'true',
+    grabcursor:'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+        0:{
+            slidesPerView: 1,
+        },
+        520:{
+            slidesPerView: 2,
+        },
+        950:{
+            slidesPerView: 3,
+        },
+    },
+  });
+  function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+ /*  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  }); */
---
+++ responsive-css-1/public_html/assets/js/script.js
@@ -0,0 +1,160 @@
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade:'true',
+  grabcursor:'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints:{
+      575:{
+        slidesPerView: 1,
+      },
+      767:{
+        slidesPerView: 2,
+      },
+      991:{
+        slidesPerView: 2,
+      },
+     1199:{
+        slidesPerView: 3,
+      },
+  },
+});
+
+/* var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade:'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints:{
+      575:{
+          slidesPerView: 1,
+      },
+      767:{
+          slidesPerView: 2,
+      },
+      991:{
+        slidesPerView: 2,
+      },
+      1199:{
+          slidesPerView: 3,
+      },
+  },
+}); */
+
+
+
+
+
+
+
+
+
+{/* <div class="Card-container swiper">
+                    <div class="carded ">
+                        <!--  -->
+                        <div class="Crd-wrapper swiper-wrapper">
+                                <div class="C-1 swiper-slide">
+                                    <div class="image-content">
+                                            <div class="Card-img">
+                                                <img src="./images/ic1.png" alt="ic1" class="Crd-img">
+                                            </div>
+                                    </div>  
+                                        <div class="crd-content">
+                                            <h3 class="c-name">Pasta+Ice-cream</h3><br>
+                                            <p class="c-dis">Lorem ipsum dolor sit amet consectetur adipisicing elit.  alias rerum dignissimos placeat.</p>
+                                            <button class="button">$34.00</button>
+                                        </div>
+                                </div>
+                        <!--  -->
+                                <div class="C-1 swiper-slide">
+                                    <div class="image-content">
+                                        <div class="Card-img">
+                                            <img src="./images/ic2.png" alt="ic2" class="Crd-img">
+                                        </div>
+                                    </div>
+                                
+                                    <div class="crd-content"> 
+                                        <h3 class="c-name">Carbonara & Pasta</h3><br>
+                                        <p class="c-dis">Lorem ipsum dolor sit amet consectetur adipisicing elit.  alias rerum dignissimos placeat.</p>
+                                        <button class="button">$34.00</button></div>
+                                </div>
+                                <!--  -->
+                                <div class="C-1 swiper-slide">
+                                    <div class="image-content">
+                                        <div class="Card-img">
+                                            <img src="./images/ic3.png" alt="ic3" class="Crd-img">
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="crd-content">
+                                        <h3 class="c-name">Mie Gyam Pangsit</h3><br>
+                                        <p class="c-dis">Lorem ipsum dolor sit amet consectetur adipisicing elit.  alias rerum dignissimos placeat.</p>
+                                        <button class="button">$34.00</button></div>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="swiper-button-next swiper-navbtn"></div>
+                    <div class="swiper-button-prev swiper-navbtn"></div>
+                    <div class="swiper-pagination"></div>
+                </div> */}
+
+
+
+               /*  <div class="Card-Container swiper">
+                        <div class="Slide-Content ">
+                            <div class="card-wrapper swiper-wrapper">
+                               
+                                <div class="Card swiper-slide">
+                                    <div class="Image-Content">
+                                        <div class="Card-Img">
+                                            <img src="./images/ic1.png" alt="ic1" class="Crd-img">
+                                        </div>
+                                    </div>
+                                    <div class="Card-Content">
+                                            <h3 class="c-name">Pasta+Ice-cream</h3><br>
+                                            <p class="c-dis">Lorem ipsum dolor sit amet consectetur adipisicing elit.  alias rerum dignissimos placeat.</p>
+                                            <button class="button">$34.00</button>
+                                    </div>
+                                </div>
+                                
+                                <div class="Card swiper-slide">
+                                    <div class="Image-Content">
+                                        <img src="./images/ic2.png" alt="ic2" class="Crd-img">
+                                    </div>
+                                    <div class="Card-Content">
+                                        <h3 class="c-name">Carbonara & Pasta</h3><br>
+                                        <p class="c-dis">Lorem ipsum dolor sit amet consectetur adipisicing elit.  alias rerum dignissimos placeat.</p>
+                                        <button class="button">$34.00</button>
+                                    </div>
+                                </div>
+                                
+                                <div class="Card swiper-slide">
+                                    <div class="Image-Content">
+                                        <img src="./images/ic3.png" alt="ic3" class="Crd-img">
+                                    </div>
+                                    <div class="Card-Content">
+                                        <h3 class="c-name">Mie Gyam Pangsit</h3><br>
+                                        <p class="c-dis">Lorem ipsum dolor sit amet consectetur adipisicing elit.  alias rerum dignissimos placeat.</p>
+                                        <button class="button">$34.00</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                                <div class="swiper-button-next swiper-navbtn"></div>
+                                <div class="swiper-button-prev swiper-navbtn"></div>
+                                <div class="swiper-pagination"></div>
+                    </div> */
